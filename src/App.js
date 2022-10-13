34618589b44c052a59965b42ead62bebc985af75
myladd <mylad.zandkarimi@gmail.com>
2022-10-14 00:23:55 +0330

    setup private routes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import {Routes, Route} from 'react-router-dom'
 import {AuthContextProvider} from "./Context/AuthContext";
 import SignIn from "./Pages/SignIn";
 import Dashboard from "./Pages/Dashboard";
+import PrivateRoutes from "./Pages/PrivateRoutes";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
               <Routes>
                   <Route path='/' element={<SignIn/>}/>
                   <Route path='/Register' element={<SignUp/>}/>
-                  <Route path='/Dashboard' element={<Dashboard/>}/>
+                  <Route path='/Dashboard' element={<PrivateRoutes><Dashboard/></PrivateRoutes>}/>
               </Routes>
           </AuthContextProvider>
 
